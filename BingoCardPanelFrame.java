535e76abf55b8fe29c39e6a9bb062d7c1a76394c
dancheung555 <90879895+dancheung555@users.noreply.github.com>
2021-09-18 21:16:00 -0500

    Update BingoCardPanelFrame.java

--- BingoCardPanelFrame.java
+++ BingoCardPanelFrame.java
@@ -3,19 +3,27 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
 import javax.swing.*;
 
 
-public class BingoCardPanelFrame extends JFrame implements ActionListener {
+public class BingoCardPanelFrame extends JFrame implements MouseListener {
 	private JLabel label1;
 	private JButton button;
 	private JTextField textField1;
 	private Container win;
+	private int[][] bingoArr = new int[5][5];
+	
+	private BallGenerator gen;
 	
 	public BingoCardPanelFrame(String framename) {
 		super(framename);
 		win = getContentPane();
+		JPanel panel = new JPanel();
 		win.setLayout(null);
+		
+		gen = new BallGenerator();
 		
 		BingoPanel screen2 = new BingoPanel();
 		screen2.setSize(715, 863);
@@ -25,8 +33,12 @@ public class BingoCardPanelFrame extends JFrame implements ActionListener {
 		generator.setSize(200, 100);
 		generator.setFont(new Font("Times New Roman", Font.PLAIN, 40));
 		generator.setLocation(800, 200);
-		generator.addActionListener(this);
+		generator.addMouseListener(this);
 		generator.setVisible(true);
+		
+		
+		
+		
 		win.add(generator);
 		
 		BingoCard x = new BingoCard();
@@ -37,9 +49,28 @@ public class BingoCardPanelFrame extends JFrame implements ActionListener {
 		
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+	public void mousePressed(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	public void mouseClicked(MouseEvent e) {
+		System.out.println("Hello");
 		
+		bingoArr = BingoCard.getBingoNumbers();
+		
+		int num = gen.getNextRandomNumber();
+		System.out.println(num);
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				if(bingoArr[i][j] == num) {
+					BingoCard.setCrossedOff(i, j);
+				}
+				
+			}
+		}
+		if(BingoCard.fiveInARow()) {
+			System.out.println(BingoCard.fiveInARow());
+		}
 	}
+	
 }
