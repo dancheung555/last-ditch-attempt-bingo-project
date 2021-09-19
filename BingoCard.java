e7c885a2aaa09b9c6d54d95486fe6dcce13f5e8a
dancheung555 <90879895+dancheung555@users.noreply.github.com>
2021-09-19 11:24:21 -0500

    Update BingoCard.java

--- BingoCard.java
+++ BingoCard.java
@@ -6,18 +6,18 @@ import javax.swing.JButton;
 
 public class BingoCard {
 	
-	private static int[][] arr = new int[5][5];
-	private static boolean[][] crossedOff = new boolean[5][5];
+	private int[][] arr = new int[5][5];
+	private boolean[][] crossedOff = new boolean[5][5];
 	
-	private static boolean won = false;
+	private boolean won = false;
 	
-	private static List<Integer> banks0 = new ArrayList<>(15);
-	private static List<Integer> banks1 = new ArrayList<>(15);
-	private static List<Integer> banks2 = new ArrayList<>(15);
-	private static List<Integer> banks3 = new ArrayList<>(15);
-	private static List<Integer> banks4 = new ArrayList<>(15);
+	private List<Integer> banks0 = new ArrayList<>(15);		//1-15
+	private List<Integer> banks1 = new ArrayList<>(15);		//16-30
+	private List<Integer> banks2 = new ArrayList<>(15);		//31-45
+	private List<Integer> banks3 = new ArrayList<>(15);		//46-60
+	private List<Integer> banks4 = new ArrayList<>(15);		//61-75
 	
-	private static Random random;
+	private Random random;
 	
 	public BingoCard() {
 		createRandom(BingoCardTextFrame.seedUpdate());
@@ -29,22 +29,98 @@ public class BingoCard {
 			}
 		}
 		
+		generateBanks();
+		generateTiles();
+	}
+	
+	
+	
+	public int[][] getArr() {
+		return arr;
+	}
+	
+	public void createRandom(long seed) {
+		random = new Random(seed);
+	}
+	
+	public int[][] getBingoNumbers() {
+		return arr;
+	}
+	
+	public boolean isCrossedOff(int i, int j) {
+		return crossedOff[i][j];
+	}
+	
+	public void setCrossedOff(int i, int j) {
+		if(!won) crossedOff[i][j] = true;
+	}
+	
+	
+	public boolean fiveInARow() {
+		if(!won) {
+			for(int i = 0; i < 5; i++) {
+				if(isCrossedOff(i, 0) && isCrossedOff(i, 1) && isCrossedOff(i, 2) &&  isCrossedOff(i, 3) && isCrossedOff(i, 3) && isCrossedOff(i, 4)) return won = true;
+			}
+			for(int i = 0; i < 5; i++) {
+				if(isCrossedOff(0, i) && isCrossedOff(1, i) && isCrossedOff(2, i) &&  isCrossedOff(3, i) && isCrossedOff(3, i) && isCrossedOff(4, i)) return won = true;
+			}
+			
+			//diagonal 1
+			if(crossedOff[0][0] && crossedOff[1][1] && crossedOff[2][2] && crossedOff[3][3] && crossedOff[4][4]) return won = true;
+			
+			//diagonal 2
+			if(crossedOff[4][0] && crossedOff[3][1] && crossedOff[2][2] && crossedOff[1][3] && crossedOff[0][4]) return won = true;
+			
+			return false;
+		} else {
+			ArrayList<Integer> arr0 = new ArrayList<>();
+			for(int i = 1; i <= 15; i++) arr0.add(i);
+			setBanks0(arr0);
+			
+			ArrayList<Integer> arr1 = new ArrayList<>();
+			for(int i = 16; i <= 30; i++) arr1.add(i);
+			setBanks1(arr1);
+			
+			ArrayList<Integer> arr2 = new ArrayList<>();
+			for(int i = 31; i <= 45; i++) arr2.add(i);
+			setBanks2(arr2);
+			
+			ArrayList<Integer> arr3 = new ArrayList<>();
+			for(int i = 46; i <= 60; i++) arr3.add(i);
+			setBanks3(arr3);
+			
+			ArrayList<Integer> arr4 = new ArrayList<>();
+			for(int i = 61; i <= 75; i++) arr4.add(i);
+			setBanks4(arr4);
+			return true;
+		}
+	}
+	
+	private void setBanks0(ArrayList<Integer> arr) {
+		banks0 = arr;
+	}
+	private void setBanks1(ArrayList<Integer> arr) {
+		banks1 = arr;
+	}
+	private void setBanks2(ArrayList<Integer> arr) {
+		banks2 = arr;
+	}
+	private void setBanks3(ArrayList<Integer> arr) {
+		banks3 = arr;
+	}
+	private void setBanks4(ArrayList<Integer> arr) {
+		banks4 = arr;
+	}
+	
+	private void generateBanks() {
 		for(int i = 1; i <= 15; i++) {
 			banks0.add(i);
 			banks1.add(i+15);
 			banks2.add(i+15*2);
 			banks3.add(i+15*3);
 			banks4.add(i+15*4);
-			
 		}
-		
-		generateTiles();
 	}
-	
-	public int[][] getArr() {
-		return arr;
-	}
-	
 	private void generateTiles() {
 		
 		for(int i = 0; i < 5; i++) {
@@ -82,64 +158,6 @@ public class BingoCard {
 			arr[i][4] = number;
 		}
 		
-	}
-	
-	
-	
-	public static void createRandom(long seed) {
-		random = new Random(seed);
-	}
-	
-	public static int[][] getBingoNumbers() {
-		return arr;
-	}
-	
-	public static boolean isCrossedOff(int i, int j) {
-		return crossedOff[i][j];
-	}
-	
-	public static void setCrossedOff(int i, int j) {
-		if(!won) crossedOff[i][j] = true;
-	}
-	
-	public static boolean fiveInARow() {
-		if(!won) {
-			for(int i = 0; i < 5; i++) {
-				if(crossedOff[i][0] && crossedOff[i][1] && crossedOff[i][2] &&  crossedOff[i][3] && crossedOff[i][3] && crossedOff[i][4]) return won = true;
-			}
-			for(int i = 0; i < 5; i++) {
-				if(crossedOff[0][i] && crossedOff[1][i] && crossedOff[2][i] &&  crossedOff[3][i] && crossedOff[3][i] && crossedOff[4][i]) return won = true;
-			}
-			
-			//diagonal 1
-			if(crossedOff[0][0] && crossedOff[1][1] && crossedOff[2][2] && crossedOff[3][3] && crossedOff[4][4]) return won = true;
-			
-			//diagonal 2
-			if(crossedOff[4][0] && crossedOff[3][1] && crossedOff[2][2] && crossedOff[1][3] && crossedOff[0][4]) return won = true;
-			
-			return false;
-		} else {
-			ArrayList<Integer> arr0 = new ArrayList<>();
-			for(int i = 1; i <= 15; i++) arr0.add(i);
-			banks0 = arr0;
-			
-			ArrayList<Integer> arr1 = new ArrayList<>();
-			for(int i = 16; i <= 30; i++) arr1.add(i);
-			banks1 = arr1;
-			
-			ArrayList<Integer> arr2 = new ArrayList<>();
-			for(int i = 31; i <= 45; i++) arr2.add(i);
-			banks2 = arr2;
-			
-			ArrayList<Integer> arr3 = new ArrayList<>();
-			for(int i = 46; i <= 60; i++) arr3.add(i);
-			banks3 = arr3;
-			
-			ArrayList<Integer> arr4 = new ArrayList<>();
-			for(int i = 61; i <= 75; i++) arr4.add(i);
-			banks4 = arr4;
-			return true;
-		}
 	}
 	
 	
