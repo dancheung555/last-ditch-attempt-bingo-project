dfa1a26d31b835acfee66a9e5e28161d240f3ec4
dancheung555 <90879895+dancheung555@users.noreply.github.com>
2021-09-19 11:24:35 -0500

    Update BingoCardPanelFrame.java

--- BingoCardPanelFrame.java
+++ BingoCardPanelFrame.java
@@ -8,12 +8,14 @@ import java.awt.event.MouseEvent;
 import javax.swing.*;
 
 
-public class BingoCardPanelFrame extends JFrame implements MouseListener {
+public class BingoCardPanelFrame extends JFrame {
 	private JLabel label1;
 	private JButton button;
 	private JTextField textField1;
 	private Container win;
 	private int[][] bingoArr = new int[5][5];
+	
+	private BingoCard x = new BingoCard();
 	
 	private BallGenerator gen;
 	
@@ -26,22 +28,10 @@ public class BingoCardPanelFrame extends JFrame implements MouseListener {
 		gen = new BallGenerator();
 		
 		BingoPanel screen2 = new BingoPanel();
-		screen2.setSize(715, 863);
-		win.add(screen2);
-		
-		JButton generator = new JButton("Generate");
-		generator.setSize(200, 100);
-		generator.setFont(new Font("Times New Roman", Font.PLAIN, 40));
-		generator.setLocation(800, 200);
-		generator.addMouseListener(this);
-		generator.setVisible(true);
+		add(screen2);
 		
 		
-		
-		
-		win.add(generator);
-		
-		BingoCard x = new BingoCard();
+//		pack();
 		
 		setSize(1100, 900);
 		setVisible(true);
@@ -49,26 +39,8 @@ public class BingoCardPanelFrame extends JFrame implements MouseListener {
 		
 	}
 
-	public void mousePressed(MouseEvent e) {}
-	public void mouseReleased(MouseEvent e) {}
-	public void mouseEntered(MouseEvent e) {}
-	public void mouseExited(MouseEvent e) {}
-	public void mouseClicked(MouseEvent e) {
-		
-		bingoArr = BingoCard.getBingoNumbers();
-		
-		int num = gen.getNextRandomNumber();
-		System.out.println(num);
-		for(int i = 0; i < 5; i++) {
-			for(int j = 0; j < 5; j++) {
-				if(bingoArr[i][j] == num) {
-					BingoCard.setCrossedOff(i, j);
-				}
-			}
-		}
-		if(BingoCard.fiveInARow()) System.out.println(BingoCard.fiveInARow());
-		
-		repaint();
-	}
+	
+
+	
 	
 }
